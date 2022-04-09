61264b1242c34ee29f2b67fb1a64176fc17417f5
blobcode <lhelme123@gmail.com>
2022-04-09 00:07:25 -0400

    comments

--- main.go
+++ main.go
@@ -11,20 +11,23 @@ import (
 	"strings"
 )
 
-const port = 8080
-
+// embedded files
 //go:embed static/* post/* templates/*
 var f embed.FS
 
-func main() {
-	static, _ := fs.Sub(f, "static")
+const port = 8080 // port to run on
 
+func main() {
+	// static files
+	static, _ := fs.Sub(f, "static")
 	fs := http.FileServer(http.FS(static))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	// handlers
 	http.HandleFunc("/post/", postHandler)
 	http.HandleFunc("/", serveTemplate)
 
+	// start
 	log.Println("listening on http://localhost:" + strconv.Itoa(port))
 	err := http.ListenAndServe("localhost:"+strconv.Itoa(port), nil)
 	if err != nil {
@@ -33,6 +36,8 @@ func main() {
 }
 
 func serveTemplate(w http.ResponseWriter, r *http.Request) {
+
+	// setup paths
 	tp, _ := fs.Sub(f, "templates")
 
 	lp := "layout.html"
@@ -41,12 +46,14 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	}
 	fp := strings.TrimPrefix(filepath.Clean(r.URL.Path), `\`)
 
+	// 404
 	_, err := tp.Open(fp)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
+	// parse templates
 	tmpl, err := template.ParseFS(tp, lp, fp)
 	if err != nil {
 		log.Println(err.Error())
@@ -62,22 +69,25 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
+
+	// setup paths
 	tp, _ := fs.Sub(f, "templates")
 
 	lp := "post-layout.html"
 	fp := filepath.Join(strings.TrimPrefix(filepath.Clean(r.URL.Path), "/"))
-	fp = strings.TrimPrefix(fp, `\`)
-	fp = strings.ReplaceAll(fp, `\`, `/`)
+	fp = strings.ReplaceAll(strings.TrimPrefix(fp, `\`), `\`, `/`)
 
 	// load the named .txt file for processing
 	txt := strings.ReplaceAll(fp, ".html", ".txt")
 
+	// 404
 	_, err := f.ReadFile(txt)
 	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
 
+	// read txt file
 	content, err := f.ReadFile(txt)
 	if err != nil {
 		log.Println(err.Error())
@@ -85,6 +95,7 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// template parsing
 	tmpl, err := template.ParseFS(tp, lp)
 	if err != nil {
 		log.Println(err.Error())
